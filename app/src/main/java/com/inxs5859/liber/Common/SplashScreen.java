13df04267c53a91321220442a802e7e4888a6e66
tmt02 <tauhidtamim0016@gmail.com>
2020-12-20 21:28:39 +0600

    added splash screen

--- app/src/main/java/com/inxs5859/liber/Common/SplashScreen.java
+++ app/src/main/java/com/inxs5859/liber/Common/SplashScreen.java
@@ -2,17 +2,50 @@ package com.inxs5859.liber.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.inxs5859.liber.R;
+import com.inxs5859.liber.User.UserDashboard;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+
+    //variables
+    ImageView backgroundImage;
+
+    //animations
+    Animation sideAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        //hooks
+        backgroundImage = findViewById(R.id.background_image);
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+
+        //set animations to elements
+        backgroundImage.setAnimation(sideAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), UserDashboard.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_TIMER);
+
     }
 }
---
+++ app/src/main/java/com/inxs5859/liber/User/UserDashboard.java
@@ -0,0 +1,16 @@
+package com.inxs5859.liber.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.inxs5859.liber.R;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
