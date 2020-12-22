278fcf6925af6b246797f5cb07c2779fa887651c
tmt02 <tauhidtamim0016@gmail.com>
2020-12-22 14:01:03 +0600

    fixed blurred splash & added license for chip navigation bar

--- app/src/main/java/com/inxs5859/liber/User/UserDashboard.java
+++ app/src/main/java/com/inxs5859/liber/User/UserDashboard.java
@@ -9,6 +9,25 @@ import android.view.WindowManager;
 import com.inxs5859.liber.R;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
+/*
+
+#for chip navigation bar
+
+MIT License
+
+Copyright (c) 2019 Ismael Di Vita
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+*/
+
 public class UserDashboard extends AppCompatActivity {
 
     ChipNavigationBar chipNavigationBar;
